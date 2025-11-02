07e8e7bcd7a39a5e633f1e8a855f48d6ecf4a049
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-11-02 13:00:17 -0500

    Creando estructura base para las rutas de la landing

---
+++ app/(landing)/contacto/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ContactoPage() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  );
+}
---
+++ app/(landing)/inicio/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function InicioPage() {
+  return (
+    <div>
+      <h1>Inicio Page</h1>
+    </div>
+  );
+}
